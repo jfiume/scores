1c41728662eb4d2e577f20848e57d81219a47b4f
Joe Fiume <joefiume88@gmail.com>
2020-09-19 23:33:54 -0700

    added a sort

--- main.asm
+++ main.asm
@@ -28,8 +28,8 @@ Scores: .byte 18, 20, 6, 10, 16, 16, 18, 19, 13, 0, 14, 16, 14, 17, 16
 numScores:
 		.word 15
 Max:	.space 2
-mode:	.space 2
-histogram:
+Mode:	.space 2
+Histogram:
 		.space 21
 
 		.text
@@ -40,6 +40,10 @@ histogram:
 	clr.b R5
 	clr.b R6
 	clr.b R7
+	clr.b R8
+	clr.b R9
+	clr.b R10
+	clr.b R11
 	mov.w #Scores, R13
 	mov.w #Scores, R14
 	add.w numScores, R14
@@ -47,18 +51,31 @@ histogram:
 	mov.b R15, Max
 	mov.w #Scores, R13
 	clr.b R15
-	;call #Sort
+	mov.b numScores, R8
+	mov.b #-1, R9
+	call #Sort
+	clr.b R5
+	clr.b R6
+	clr.b R7
+	clr.b R8
 loop:
 	mov.b @R13, R12
 	push R13
 	call #histo
-	mov.b R15, @R7(histogram)
-	clr.b R15
+	mov.b R15, @R7(Histogram)
 	inc.b R7
 	pop R13
-	inc.w R13
-	cmp.w R13, R14
-	jnz loop
+	add.w R15, R13
+	clr.b R15
+	cmp.w R14, R13
+	jl loop
+
+	mov.w #Histogram, R13
+	mov.w #Histogram, R14
+	add.w numScores, R14
+	call #max
+	mov.b R15, Mode
+	jmp $
 
 max:
 	cmp.w R13, R14
@@ -88,18 +105,31 @@ returnHisto:
 	ret
 
 
-
-
 Sort:
-	clr.b R15
-	call #max
-	;mov.b R15, sortedScores
-	inc.w R6
-	add.w #Scores, R6
-	mov.w R6, R13
-	inc.b R5
-	cmp numScores, R5
-	jl Sort
+	tst.b R9		;test if R4 is 0
+	jz    returnSort		;if R4 is 0 then jump to the end, the list is sorted
+	mov.b #0, R7	;move 0x00 to R7, this will serve as index 1
+	mov.b #1, R8	;move 0x01 to R8, this will serve as index 2
+	mov.b #0, R9	;move 0x00 to R4, this will count the number of swaps though the list
+
+next:
+	mov.b @R7(Scores), R5		;move the element in the list corresponding to the index in R7 to R5
+	mov.b @R8(Scores), R6		;move the element in the list corresponding to the index in R8 to R6
+	cmp.b R5, R6			;compare list element i to i+1
+	jge   incNums			;if R6 is larger or equal to R5 then we jump to incrementing the indexes, no swaps are made
+
+swapNums:
+	mov.b R6, @R7(Scores)		;swap the elements of the list, i -> i+1
+	mov.b R5, @R8(Scores)		;swap the elements of the list, i+1 -> i
+	inc.b R9				;add one to the count of swaps
+
+incNums:
+	inc.b R7				;increment index 1
+	inc.b R8				;increment index 2
+	cmp.b R8, numScores		;compare index 2 to the total number of elements in the list
+	jz    Sort				;if it is 0 then we have reached the end of the list and jump back to the beginning
+	jmp   next				;if we have not reached the end of the list, then we continue to compare the next two elements
+returnSort:
 	ret
 
 
